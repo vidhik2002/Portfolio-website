77c273e4bb53f9e2b11f63d4886849a79e5f0e6b
vidhik2002 <vidhik2002@gmail.com>
2021-10-21 14:48:46 +0530

    feat: added content.js

---
+++ portfolio/content.js
@@ -0,0 +1,94 @@
+var content = {
+    "name": "Vidhi Khaitan",
+    "description": "Learning and Developing!",
+    "button": "View Work",
+    "onelinedescription":"Im a developer",
+    "aboutdescription": "My name is Vidhi Khaitan. I'm a CSE student studying in Vellore Institute of Technology, Vellore. I try to learn something new everday and aspire to be a full stack developer.I have worked with frontend and backend. My aim for this year would be to contribute to more open source projects.Do checkout my work below!",
+    "contactdescription":"",
+    "address": "Mumbai, Maharashtra",
+    "phone": "7506325241",
+    "email": "vidhik2002@gmail.com"
+}
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Vite App</title>
+//     <style>
+//       body {
+//         margin:0;
+//       }
+//       .absolute{
+//         position:absolute;
+//       }
+//       .overlay {
+//          position: absolute;
+//           display: flex;
+//           flex-direction: column;
+//           align-items: center;
+//           justify-content: center;
+//           min-height: 100vh;
+//           min-width: 100vw;
+//           color: white;
+//           font-family:'Courier New', Courier, monospace;
+//           font-weight: bold;
+//           font-size: 3rem;
+//       }
+//       button {
+//           border: none;
+//           background-color: rgb(2, 1, 61);
+//           color: white;
+//           font-size: 15px;
+//           width: 20%;
+//           height: 20%;
+//           padding: 15px;
+//           border-radius: 15px;
+//       }
+//       button:hover {
+//           border-color: black;
+//           background-color: black;
+//       }
+//       h1 {
+//         padding-bottom: 0;
+//         margin-bottom: 0;
+//       }
+//     </style>
+//     <script src="content.js"></script>
+//   </head>
+//   <body>
+//     <!-- <div id="app">
+//       <div class="overlay">
+//         <p id="name"></p>
+//         <p id="description"></p>
+//         <button id="button"></button>
+//         <button id="button">
+//             <a href="#">
+//                 <span></span>
+//                 <span></span>
+//                 <span></span>
+//                 <span></span>
+//             </a>
+//         </button>
+//       </div>
+//     </div> -->
+//     <div id="app">
+//       <div
+//       class="overlay">
+//         <p>Vidhi Khaitan</p>
+//         <p id="description">random description</p>
+//       <!-- <a
+//       class="border px-4 py-2 rounded-lg text-sm font-space-mono uppercase mt-8 hover:bg-white hover:text-gray-800 inline-block"
+//       > -->
+//       <button id="button">
+//               View work
+//       </button>
+//     <!-- </a> -->
+//   </div>
+
+// </div>
+//     <script type="module" src="/main.js"></script>
+//   </body>
+// </html>
