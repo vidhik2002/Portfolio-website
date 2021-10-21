1519175665163a5de60de33dd96624d314566e7e
vidhik2002 <vidhik2002@gmail.com>
2021-10-21 22:32:59 +0530

    feat: added styles

--- portfolio/content.js
+++ portfolio/content.js
@@ -1,12 +1,12 @@
 var content = {
-    "name": "Vidhi Khaitan",
-    "description": "Learning and Developing!",
-    "button": "View Work",
-    "onelinedescription":"I'm A Developer",
-    "aboutdescription": "My name is Vidhi Khaitan. I'm a 2nd-year CSE student studying in Vellore Institute of Technology, Vellore. I am a self-taught developer, trying to learn something new everday and aspire to be a full stack developer.I have worked with frontend and backend. My aim for this year would be to contribute to more open source projects.Do checkout my work below!",
-    "contactdescription":"",
-    "address": "Mumbai, Maharashtra",
-    "phone": "7506325241",
-    "email": "vidhik2002@gmail.com"
-}
-
+  name: "Vidhi Khaitan",
+  description: "Learning and Developing!",
+  button: "View Work",
+  onelinedescription: "I'm A Developer",
+  aboutdescription:
+    "My name is Vidhi Khaitan. I'm a 2nd-year CSE student studying in Vellore Institute of Technology, Vellore. I am a self-taught developer, trying to learn something new everday and aspire to be a full stack developer.I have worked with frontend and backend. My aim for this year would be to contribute to more open source projects.Do checkout my work below!",
+  contactdescription: "",
+  address: "Mumbai, Maharashtra",
+  phone: "7506325241",
+  email: "vidhik2002@gmail.com",
+};
